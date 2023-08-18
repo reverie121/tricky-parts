f21641deea7d624e5bb74d7d01dbc331bda37380
Jeremy Sobczak <code@jerspace.com>
2023-08-18 13:26:26 -0400

    Added curried-add.

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,12 @@
-function curriedAdd(total) {
+// Write a function called curriedAdd. If you give this function a number, it returns a new function to you. If you give this function no arguments, it returns the total of all the numbers you’ve passed to it so far.
 
+function curriedAdd(total) {
+    if (total === undefined) return 0;
+    if (total !== undefined) return function addToTotal(num) {
+        if (num === undefined) return total;
+        total += num;
+        return addToTotal;
+    }
 }
 
 module.exports = { curriedAdd };
