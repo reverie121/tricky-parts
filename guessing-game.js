0181f87c7ef32d83112859ee0896be5157bb93bd
Jeremy Sobczak <code@jerspace.com>
2023-08-18 12:50:44 -0400

    Initialized npm. Added guessing-game.

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,22 @@
 function guessingGame() {
-
+    let num = Math.floor(Math.random() * 100);
+    gameIsActive = true;
+    guessCount = 0;
+    console.log("num is" + num)
+    return function game(guess) {
+        if (gameIsActive) {
+            guessCount++;
+            if (guess < num) return `${guess} is too low!`
+            else if (guess > num) return `${guess} is too high!`
+            else {
+                gameIsActive = false;
+                return `You win! You found ${num} in ${guessCount} guesses.`
+            }
+        }
+        else {
+            return "The game is over, you already won!"
+        }
+    }
 }
 
 module.exports = { guessingGame };
